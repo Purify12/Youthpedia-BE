92030b32ab3d94e618b5da7815772b00df89b4c0
Purify12 <marchelsitumorang2000@gmail.com>
2022-01-15 11:04:00 +0700

    make it better

--- index.js
+++ index.js
@@ -45,6 +45,16 @@ app.use(
     path: [
       { url: "/users/login", methods: ["POST"] },
       { url: "/users/register", methods: ["POST"] },
+      { url: "/artikel/get", methods: ["GET"] },
+      { url: "/artikel/get/:id", methods: ["GET"] },
+      { url: "/webinar/get", methods: ["GET"] },
+      { url: "/webinar/get/:id", methods: ["GET"] },
+      { url: "/comment/get", methods: ["GET"] },
+      { url: "/comment/get/:id", methods: ["GET"] },
+      { url: "/category/get", methods: ["GET"] },
+      { url: "/category/get/:id", methods: ["GET"] },
+      { url: "/", methods: ["GET"] },
+
     ],
   })
 );
@@ -52,12 +62,13 @@ app.use(
 app.use(express.json());
 
 // initialize routes 
+
 app.use("/users", require("./routes/users.routes"));
-app.use("/artis", require("./routes/artikel"));
 app.use("/webinar", require("./routes/webinar"));
 app.use("/artikel", require("./routes/artikel"));
 app.use("/comment", require("./routes/comment"));
 app.use("/category", require("./routes/category"));
+app.use("/", require("./routes/index"));
 // middleware for error responses
 app.use(errors.errorHandler);
 
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ const express = require("express")
 // creates a new router instance.
 const router = express.Router()
 
-router.get("/ping", (req, res) => {
+router.get("/", (req, res) => {
     const ready = {
         status: "server is ready"
     }
-
     res.status(200).send(ready)
+    res.send({ welcome: "Welcome to youthPedia API"})   
 })
-
 
 module.exports = router
